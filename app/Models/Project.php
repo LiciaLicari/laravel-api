7bca98a1853996d9d043231ffe304c5d65ada07e
“LiciaLicari” <licianbsplicari@gmail.com>
2023-11-17 15:28:06 +0100

    Initial Commit

---
+++ app/Models/Project.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
+use App\Models\Technology;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'cover_image', 'type_id', 'github', 'link', 'technology'];
+
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+}
