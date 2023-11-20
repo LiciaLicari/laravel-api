ddb043b23b9cb43aeb9a78c84620443d807edb98
“LiciaLicari” <licianbsplicari@gmail.com>
2023-11-20 18:30:09 +0100

    add on project controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,35 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\Type;
+use App\Models\Technology;
 
 class ProjectController extends Controller
 {
-    //
+    public function projects()
+    {
+
+
+        return response()->json([
+            'success' => true,
+            'result' => Project::all()
+        ]);
+    }
+
+    public function type()
+    {
+        return response()->json([
+            'success' => true,
+            'result' => Type::all(),
+        ]);
+    }
+
+    public function technologies()
+    {
+        return response()->json([
+            'success' => true,
+            'result' => Technology::all(),
+        ]);
+    }
 }
